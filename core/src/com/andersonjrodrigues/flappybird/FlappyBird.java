566bb30610b44193bef34a67116be2723b0343c5
Anderson Junior Rodrigues <andersonkz@ymail.com>
2017-09-11 00:50:51 -0300

    Adicionando imagens no jogo

--- core/src/com/andersonjrodrigues/flappybird/FlappyBird.java
+++ core/src/com/andersonjrodrigues/flappybird/FlappyBird.java
@@ -7,27 +7,28 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FlappyBird extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+
+	private SpriteBatch batch;
+	private Texture passaro;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		passaro = new Texture("passaro1.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+
+		batch.draw(passaro, 335, 600);
+
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
+		passaro.dispose();
 		batch.dispose();
-		img.dispose();
 	}
 }
